7d67dc30ca70b71074afaf59e16daf57109da15b
Abdullah Alhelal <abdullah.h@alameensoft.com>
2025-08-20 01:38:49 -0700

    add IExternalService

---
+++ clean architecture.Core/Interfaces/IExternalService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clean_architecture.Core.Interfaces
+{
+    public interface IExternalService
+    {
+
+        ValueTask<TResult> Post<TResult,TRequest>(string url ,TRequest Content);
+        ValueTask<HttpResponseMessage> Post<TRequest>(string url ,TRequest Content);
+        ValueTask<TResult> Get<TResult>(string url);
+        ValueTask<TResult> Put<TResult, TRequest>(string url, TRequest Content);
+        ValueTask<HttpResponseMessage> Put<TRequest>(string url, TRequest Content);
+        ValueTask<HttpResponseMessage> Delete<TResult>(string url);
+    }
+}
